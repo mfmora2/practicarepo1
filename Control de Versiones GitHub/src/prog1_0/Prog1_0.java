d639405f59c2e05f93383057ddeed18ac1ede3b7
Manuel Mora <Manuel Mora@192.168.1.38>
2013-01-23 14:04:54 +0100

    Prog1_0

---
+++ Control de Versiones GitHub/src/prog1_0/Prog1_0.java
@@ -0,0 +1,19 @@
+package prog1_0;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Prog1_0 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leerradio = new Scanner(System.in);
+		int radio;
+		System.out.println("Introduce el radio de la esfera");
+		radio=leerradio.nextInt();
+
+	}
+
+}
